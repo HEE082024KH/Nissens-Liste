c63cb988a9599fb30e601ebecce8c1c1ad6bfe63
Amaliebra <jba082024@gmail.com>
2024-12-10 12:12:18 +0100

    fixing homeadress

--- attributes/HomeAdress.cs
+++ attributes/HomeAdress.cs
@@ -2,64 +2,67 @@ namespace SantasListGenerator.Attributes
 {
     public class HomeAdress
     {
-        switch (homeAdress)
+        public void godBadAdress(string homeAdress)
         {
-            //Good neighbourhoods \/
-            case "Kalfarveien";
-            case "Abels gate";
-            case "Absalon Beyers gate";
-            case "Allégaten";
-            case "Allehelgens gate";
-            case "Amalie Skrams vei";
-            case "Arbeidergaten";
-            case "Armauer Hansens vei";
-            case "Arne Garborgs gate";
-            case "Arnoldus Reimers' gate";
-            case "Asbjørnsens gate";
-            case "Astrups vei";
-            case "Bispengsgaten";
-            case "Bjerregårds gate";
-            case "Blaauws vei";
-            case "Bredsgården";
-            case "Breistølen";
-            case "Breiviksveien";
-            case "Bryggen";
-            case "C. Sundts gate";
-            case "Christian Michelsens gate";
-            case "Christies gate";
-            case "Cort Piil-Smauet";
-            case "Damsgårdsveien";
-            case "Dreggsallmenningen";
-            case "Finnegårdsgaten";
-            case "Fortunen";
-            case "Hans Hauges gate";
-            case "Haugeveien";
-            case "Jonas Lies vei";
-            case "Kaigaten";
-            case "Kirkegaten";
-            case "Klosteret";
-            case "Kong Oscars gate";
-                naughtyOrNice += 10;
-                break;
-            //bad neighbourhoods \/
-            case "Aad Gjelles gate";
-            case "Adolph Bergs vei";
-            case "Agnes Mowinckels gate";
-            case "Asylplassen";
-            case "Baglergaten";
-            case "Baneveien";
-            case "Bankgaten";
-            case "Bjørnsons gate";
-            case "Bontelabo";
-            case "Bradbenken";
-            case "Bredalsmarken";
-            case "Bøhmergaten";
-            case "Engen";
-            case "Fabrikkgaten";
-            case "Fjøsangerveien";
-            case "Hollendergaten";
-                naughtyOrNice -= 10;
-                break;
+            switch (homeAdress)
+            {
+                //Good neighbourhoods \/
+                case "Kalfarveien":
+                case "Abels gate":
+                case "Absalon Beyers gate":
+                case "Allégaten":
+                case "Allehelgens gate":
+                case "Amalie Skrams vei":
+                case "Arbeidergaten":
+                case "Armauer Hansens vei":
+                case "Arne Garborgs gate":
+                case "Arnoldus Reimers' gate":
+                case "Asbjørnsens gate":
+                case "Astrups vei":
+                case "Bispengsgaten":
+                case "Bjerregårds gate":
+                case "Blaauws vei":
+                case "Bredsgården":
+                case "Breistølen":
+                case "Breiviksveien":
+                case "Bryggen":
+                case "C. Sundts gate":
+                case "Christian Michelsens gate":
+                case "Christies gate":
+                case "Cort Piil-Smauet":
+                case "Damsgårdsveien":
+                case "Dreggsallmenningen":
+                case "Finnegårdsgaten":
+                case "Fortunen":
+                case "Hans Hauges gate":
+                case "Haugeveien":
+                case "Jonas Lies vei":
+                case "Kaigaten":
+                case "Kirkegaten":
+                case "Klosteret":
+                case "Kong Oscars gate":
+                    naughtyOrNice += 10;
+                    break;
+                //bad neighbourhoods \/
+                case "Aad Gjelles gate":
+                case "Adolph Bergs vei":
+                case "Agnes Mowinckels gate":
+                case "Asylplassen":
+                case "Baglergaten":
+                case "Baneveien":
+                case "Bankgaten":
+                case "Bjørnsons gate":
+                case "Bontelabo":
+                case "Bradbenken":
+                case "Bredalsmarken":
+                case "Bøhmergaten":
+                case "Engen":
+                case "Fabrikkgaten":
+                case "Fjøsangerveien":
+                case "Hollendergaten":
+                    naughtyOrNice -= 10;
+                    break;
+            }
         }
-}
+    }
 }
--- attributes/bools.cs
+++ attributes/bools.cs
@@ -2,25 +2,28 @@ namespace SantasListGenerator.Attributes
 {
   public class Bools
   {
+    
     public bool toiletPaperOutward;
     public bool donatesToCharity;
     public bool washesHands;
     public int naughtyOrNice;
 
+    //switch must be inside a method, constructor, or property
+
     switch (toiletPaperOutward)
     {
       case true:
-        naughtyOrNice = +10;
+        naughtyOrNice += 10;
         break;
       case false:
-        naughtyOrNice = -10;
+        naughtyOrNice -= 10;
         break;
     }
 
     switch (donatesToCharity)
     {
       case true:
-        naughtyOrNice = +20
+        naughtyOrNice += 20;
         break;
       case false:
         break;
@@ -31,7 +34,7 @@ namespace SantasListGenerator.Attributes
       case true:
         break;
       case false:
-        naughtyOrNice = -20
+        naughtyOrNice += 20;
         break;
     }
   }
