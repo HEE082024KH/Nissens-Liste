b970e0bf29cb2c8b47e5dc8ca3289ab5f253266c
hee082024kh <hee082024@gmail.com>
2024-12-11 10:00:27 +0100

    Updated person output

--- Program.cs
+++ Program.cs
@@ -22,12 +22,13 @@ class Program
 
         // Step 3: Initialize a variable to hold the "naughty or nice" score
         bools.naughtyOrNice = 0;
+
         Console.WriteLine(people); //test to see if it writes out the attributes
         if (people != null)
         {
             foreach (var person in people)
             {
-                Console.WriteLine($"Name: {person.name}, Address: {person.homeAdress}");
+                Console.WriteLine($"Name: {person.name}, Address: {person.homeAdress}, Washes Hands: {person.washesHands}, Toilet Paper Out or In: {person.toiletPaperOutward}, Donates to charity: {person.donatesToCharity}, Car Model: {person.carModel}, Music Genres: {person.musicGenres} ");
             }
         }
     }
