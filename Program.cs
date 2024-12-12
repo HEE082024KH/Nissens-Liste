b81683011ce54f1b2c97b64535106282b3c6603d
hee082024kh <hee082024@gmail.com>
2024-12-12 12:56:14 +0100

    Fixed possible null reference

--- Program.cs
+++ Program.cs
@@ -69,6 +69,9 @@ class Program
             }
         }
         var manager = new SantasListManager();
-        manager.EvaluateAndAssign(people);
+        if (people != null)
+        {
+            manager.EvaluateAndAssign(people);
+        }
     }
 }
